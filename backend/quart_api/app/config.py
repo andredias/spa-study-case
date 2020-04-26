a95f30e4fc302054487690ecd1f9ecce7cc1646c
André Felipe Dias <andre.dias@pronus.io>
2020-04-26 17:55:39 -0300

    include httpie to be used in the command line and reorder config.py

--- backend/quart_api/app/config.py
+++ backend/quart_api/app/config.py
@@ -5,10 +5,14 @@ from tempfile import gettempdir
 # .env was already read in create_app
 
 APP_NAME = 'Quart API'
+
 ENV = os.environ['ENV'].lower()
-SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(64)
+assert ENV in ('development', 'testing', 'production')
 TESTING = ENV == 'testing'
 DEBUG = ENV != 'production'
+
 LOG_LEVEL = DEBUG and 'DEBUG' or 'INFO'
 LOG_BACKTRACE = DEBUG
 LOG_FILENAME = Path(gettempdir(), 'quart_api.log')
+
+SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(64)
