45c52b1113b25a0f0b218660f9c5c28b2386a733
André Felipe Dias <andre.dias@pronus.io>
2020-05-19 22:37:54 -0300

    improve quart_api config to use secrets library and type hints

--- backend/quart_api/app/config.py
+++ backend/quart_api/app/config.py
@@ -1,17 +1,18 @@
 import os
+import secrets
 from pathlib import Path
 from tempfile import gettempdir
 
-APP_NAME = 'Quart API'
+APP_NAME: str = 'Quart API'
 
-ENV = os.environ['ENV'].lower()
+ENV: str = os.environ['ENV'].lower()
 if ENV not in ('development', 'testing', 'production'):
     raise ValueError(f'ENV="{ENV}" but it should be "development", "testing" or "production"')
-TESTING = ENV == 'testing'
-DEBUG = ENV != 'production'
+TESTING: bool = ENV == 'testing'
+DEBUG: bool = ENV != 'production'
 
-LOG_LEVEL = os.getenv('LOG_LEVEL') or DEBUG and 'DEBUG' or 'INFO'
-LOG_BACKTRACE = DEBUG
-LOG_FILENAME = Path(gettempdir(), 'quart_api.log')
+LOG_LEVEL: str = os.getenv('LOG_LEVEL') or DEBUG and 'DEBUG' or 'INFO'
+LOG_BACKTRACE: bool = DEBUG
+LOG_FILENAME: Path = Path(gettempdir(), 'quart_api.log')
 
-SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(64)
+SECRET_KEY: str = os.getenv('SECRET_KEY') or secrets.token_urlsafe(32)
