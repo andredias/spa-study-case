42035b5c749cbe7d5448c3620c65e7917e3a3721
André Felipe Dias <andre.dias@pronus.io>
2020-05-23 18:50:20 -0300

    fix: wait_until_responsive is not triggering Timeout

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -11,12 +11,13 @@ def wait_until_responsive(timeout: float = 3.0) -> None:
     now = ref
     while (now - ref) < timeout:
         try:
-            response = get('https://localhost/api/hello', verify=False)
+            response = get('https://localhost', verify=False)
             if response.status_code == 200:
                 return
         except NetworkError:
             pass
         sleep(0.1)
+        now = time()
     raise TimeoutError()
 
 
@@ -25,8 +26,8 @@ def docker_compose() -> None:
     filename = Path(__file__).parent.parent / 'docker-compose.yml'
     docker_compose_up = f'docker-compose -f {filename} up -d'
     check_call(docker_compose_up, shell=True)
-    wait_until_responsive()
     try:
+        wait_until_responsive()
         yield
     finally:
         check_call(['docker-compose', 'down'])
