1dc5e790ddbdf328a8d292bafe6b6771d5ffdde9
André Felipe Dias <andre.dias@pronus.io>
2020-05-24 20:18:19 -0300

    tornado_api: not log to file anymore

--- backend/tornado_api/app/main.py
+++ backend/tornado_api/app/main.py
@@ -1,21 +1,30 @@
 import sys
+from pathlib import Path
+from typing import Union
 
+from dotenv import load_dotenv
 from loguru import logger
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 
-from . import config
 from .api import HelloHandler
 
 
-def create_app():
+def create_app(env_filename: Union[str, Path] = '.env') -> Application:
+
+    # a .env file is not mandatory.
+    # You can specify envvar parameters by other means
+    # as long as they are available before config is imported
+    load_dotenv(env_filename)
+
+    from . import config
+
     # config logger
     logger.remove()  # remove standard handler
     logger.add(
-        sys.stderr, level=config.LOG_LEVEL, colorize=True, backtrace=config.LOG_BACKTRACE, enqueue=True
+        sys.stderr, level=config.LOG_LEVEL, colorize=True, backtrace=config.DEBUG, enqueue=True
     )  # reinsert it to make it run in a different thread
-    logger.add(config.LOG_FILENAME, level=config.LOG_LEVEL, backtrace=config.LOG_BACKTRACE, enqueue=True)
     logger.debug({key: getattr(config, key) for key in dir(config) if key == key.upper()})
 
     # Application setup
--- backend/tornado_api/tests/conftest.py
+++ backend/tornado_api/tests/conftest.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+
 from pytest import fixture
 from tornado.web import Application
 
 from app.main import create_app  # isort:skip
 
 
-@fixture
+@fixture(scope='session')
 def app() -> Application:
-    return create_app()
+    # Since there is a `from . import config` in create_app,
+    # this import is done just once,
+    # even if create_app is called several times.
+    app = create_app(Path(__file__).parent / 'env.test')
+    # one workaround would be to reload config import here
+    return app
