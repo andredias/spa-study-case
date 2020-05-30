fab19a214794fe58e33c552de215d9585a0da3eb
André Felipe Dias <andre.dias@pronus.io>
2020-05-30 14:57:06 -0300

    fastapi_api: new config module with init feature

--- backend/fastapi_api/app/__init__.py
+++ backend/fastapi_api/app/__init__.py
@@ -2,32 +2,29 @@ import sys
 from pathlib import Path
 from typing import Union
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from loguru import logger
 
+from . import config
 from .routers import router
 
 
 def create_app(env_filename: Union[str, Path] = '.env') -> FastAPI:
+    config.init(env_filename)
+    setup_logger()
+    app = FastAPI()
+    app.include_router(router)
+    return app
 
-    # a .env file is not mandatory.
-    # You can specify envvar parameters by other means
-    # as long as they are available before config is imported
-    load_dotenv(env_filename)
 
+def setup_logger():
+    '''
+    Configure Loguru's logger
+    '''
     from . import config
 
-    # config logger
     logger.remove()  # remove standard handler
     logger.add(
         sys.stderr, level=config.LOG_LEVEL, colorize=True, backtrace=config.DEBUG, enqueue=True
     )  # reinsert it to make it run in a different thread
     logger.debug({key: getattr(config, key) for key in dir(config) if key == key.upper()})
-
-    # App
-    app = FastAPI()
-
-    app.include_router(router)
-
-    return app
--- backend/fastapi_api/tests/conftest.py
+++ backend/fastapi_api/tests/conftest.py
@@ -9,11 +9,7 @@ from app import create_app  # isort:skip
 
 @fixture(scope='session')
 def app() -> FastAPI:
-    # Since there is a `from . import config` in create_app,
-    # this import is done just once,
-    # even if create_app is called several times.
     app = create_app(Path(__file__).parent / 'env.test')
-    # one workaround would be to reload config import here
     return app
 
 
