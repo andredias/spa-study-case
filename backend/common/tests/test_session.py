80cfbb59e4e35684a15f4b871cc82822b3527d30
André Felipe Dias <andre.dias@pronus.io>
2020-08-08 22:41:49 -0300

    common: move tests from fastapi_api to common directory

--- backend/common/tests/test_session.py
+++ backend/common/tests/test_session.py
@@ -2,6 +2,7 @@ from unittest.mock import AsyncMock, patch
 
 from fastapi import Depends, FastAPI
 from httpx import AsyncClient
+from loguru import logger
 
 from app.sessions import (  # isort:skip
     authenticated_session, create_csrf, create_session, delete_session, get_session, is_valid_csrf, session_exists
@@ -11,16 +12,16 @@ from app.sessions import (  # isort:skip
 async def test_session(app: FastAPI) -> None:
     data = {'user_id': 1}
 
-    # create session
+    logger.info('create session')
     session_id = await create_session(data)
     assert session_id
     assert await session_exists(session_id)
 
-    # get_session
+    logger.info('get_session')
     resp_data = await get_session(session_id)
     assert resp_data == data
 
-    # delete_session
+    logger.info('delete_session')
     await delete_session(session_id)
     resp_data = await get_session(session_id)
     assert resp_data is None
@@ -56,34 +57,34 @@ async def test_authenticated_session(app: FastAPI, client: AsyncClient):
     cookies = {'session_id': session_id}
     headers = {'x-csrf-token': csrf_token}
 
-    # ok: correct cookie and header
+    logger.info('ok: correct cookie and header')
     resp = await client.get('/test_session', headers=headers, cookies=cookies)
     assert resp.status_code == 200
     assert resp.json() == data
 
-    # no cookies or headers
+    logger.info('no cookies or headers')
     resp = await client.get('/test_session')
     assert resp.status_code == 401
 
-    # only cookies
+    logger.info('only cookies')
     resp = await client.get('/test_session', cookies=cookies)
     assert resp.status_code == 401
 
-    # only headers
+    logger.info('only headers')
     resp = await client.get('/test_session', headers=headers)
     assert resp.status_code == 401
 
-    # correct cookie, but wrong header
+    logger.info('correct cookie, but wrong header')
     wrong_header = {'x-csrf-token': '0' + csrf_token}
     resp = await client.get('/test_session', headers=wrong_header, cookies=cookies)
     assert resp.status_code == 401
 
-    # x-csrf-token via cookie
+    logger.info('x-csrf-token via cookie')
     wrong_cookies = cookies.copy().update({'x-csrf-token': csrf_token})
     resp = await client.get('/test_session', cookies=wrong_cookies)
     assert resp.status_code == 401
 
-    # correct cookies and headers, but session does not exist anymore
+    logger.info('correct cookies and headers, but session does not exist anymore')
     await delete_session(session_id)
     resp = await client.get('/test_session', headers=headers, cookies=cookies)
     assert resp.status_code == 401
